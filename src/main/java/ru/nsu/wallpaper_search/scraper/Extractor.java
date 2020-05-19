02003c8bd985342d15f56c19337e32c692173dc5
Denis Protasov <d.protasov@g.nsu.ru>
2020-05-20 03:55:53 +0700

    created Extractor class with private fields for json scraping

---
+++ src/main/java/ru/nsu/wallpaper_search/scraper/Extractor.java
@@ -0,0 +1,7 @@
+package ru.nsu.wallpaper_search.scraper;
+
+public class Extractor {
+    private static final String USERAGENT = "Mozilla/5.0 (Android 8.1.0; Mobile; rv:61.0) Gecko/61.0 Firefox/61.0";
+    private static final String REFERRER = "http://www.google.com";
+    private static final String YANDEX = "https://yandex.ru/images/search?text=%s&isize=eq&iw=%s&ih=%s";
+}
