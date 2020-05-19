6bf1d0e11d3de0a7481b4bc7fa197e2d953232e1
Denis Protasov <d.protasov@g.nsu.ru>
2020-05-20 05:28:48 +0700

    added module-info.java

---
+++ src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module ru.nsu.wallpaper_search.gui {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires com.sun.jna;
+    requires com.sun.jna.platform;
+    requires org.json;
+    requires org.jsoup;
+
+    opens ru.nsu.wallpaper_search.gui to javafx.fxml;
+    exports ru.nsu.wallpaper_search.gui;
+}
