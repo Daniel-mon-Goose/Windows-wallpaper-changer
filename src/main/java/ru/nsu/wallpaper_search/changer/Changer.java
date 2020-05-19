c5b029ceebbccba277b2e75f8868cdd18c4d3f37
Denis Protasov <d.protasov@g.nsu.ru>
2020-05-20 05:01:38 +0700

    removed unnecessary HashMap generics classes withn brackets

--- src/main/java/ru/nsu/wallpaper_search/changer/Changer.java
+++ src/main/java/ru/nsu/wallpaper_search/changer/Changer.java
@@ -22,7 +22,7 @@ public class Changer {
         long SPIF_UPDATEINIFILE = 0x01;
         long SPIF_SENDWININICHANGE = 0x02;
 
-        SPI INSTANCE = Native.loadLibrary("user32", SPI.class, new HashMap<String, Object>() {
+        SPI INSTANCE = Native.loadLibrary("user32", SPI.class, new HashMap<>() {
             {
                 put(OPTION_TYPE_MAPPER, W32APITypeMapper.UNICODE);
                 put(OPTION_FUNCTION_MAPPER, W32APIFunctionMapper.UNICODE);
