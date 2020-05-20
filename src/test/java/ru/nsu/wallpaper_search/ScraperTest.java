e83673723f2f0bea3a256230b545495208a5ae3d
Denis Protasov <d.protasov@g.nsu.ru>
2020-05-20 16:51:05 +0700

    added yandex pictures page access test

--- src/test/java/ru/nsu/wallpaper_search/ScraperTest.java
+++ src/test/java/ru/nsu/wallpaper_search/ScraperTest.java
@@ -5,6 +5,7 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.junit.Assert;
 import org.junit.Test;
+import ru.nsu.wallpaper_search.scraper.Extractor;
 
 import java.io.IOException;
 
@@ -29,6 +30,17 @@ public class ScraperTest {
             }
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void accessPage() throws IOException {
+        try {
+            var doc = Extractor.accessPicturesPage("1920x1080", "muffin");
+            System.out.println();
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw e;
         }
     }
 }
