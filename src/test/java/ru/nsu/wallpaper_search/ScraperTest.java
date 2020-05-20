fd918510366b2c84b11e2c2369645dd8d9203444
Denis Protasov <d.protasov@g.nsu.ru>
2020-05-20 16:44:06 +0700

    added jsoup library test

---
+++ src/test/java/ru/nsu/wallpaper_search/ScraperTest.java
@@ -0,0 +1,34 @@
+package ru.nsu.wallpaper_search;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.IOException;
+
+public class ScraperTest {
+    @Test
+    public void jsoupWebsiteTest() {
+        try {
+            Document doc = Jsoup.connect("https://wallhaven.cc/")
+                    .userAgent("Chrome/4.0.249.0 Safari/532.5")
+                    .referrer("http://www.google.com")
+                    .get();
+
+            Element firstLine = doc.selectFirst("div.more-feat").child(0);
+
+            Assert.assertNotEquals(firstLine.children().size(), 0);
+
+            for (var elem: firstLine.children()) {
+                var dat = elem.selectFirst("img").attr("src");
+                System.out.println(dat.replaceFirst("th", "w")
+                        .replaceFirst("small", "full")
+                        .replaceAll("(.{6})\\.jpg", "wallhaven-$1.jpg"));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
