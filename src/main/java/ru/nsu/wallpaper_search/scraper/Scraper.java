2225d71340a5d48eba828150a736e2ee269478b5
Denis Protasov <d.protasov@g.nsu.ru>
2020-05-20 15:47:24 +0700

    added responding to user request and sending a response

--- src/main/java/ru/nsu/wallpaper_search/scraper/Scraper.java
+++ src/main/java/ru/nsu/wallpaper_search/scraper/Scraper.java
@@ -1,4 +1,16 @@
 package ru.nsu.wallpaper_search.scraper;
 
+import ru.nsu.wallpaper_search.tools.PicCell;
+
+import java.io.IOException;
+import java.util.List;
+
 public class Scraper {
+    public List<PicCell> respondWithQuery(String size, String theme) throws IOException {
+        var doc = Extractor.accessPicturesPage(size, theme);
+        var result = Extractor.getPictures(doc);
+        result.addAll(Extractor.getPictures(doc));
+
+        return result;
+    }
 }
