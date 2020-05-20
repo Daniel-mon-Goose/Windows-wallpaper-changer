ca0ed0e8091e26b6944973c94cee3c01472cc8f9
Alena Kokorina <alena.kokorina24@gmail.com>
2020-05-21 02:05:01 +0700

    added scene creating and fxml loading

--- src/main/java/ru/nsu/wallpaper_search/gui/Gui.java
+++ src/main/java/ru/nsu/wallpaper_search/gui/Gui.java
@@ -1,4 +1,34 @@
 package ru.nsu.wallpaper_search.gui;
 
-public class Gui {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Gui extends Application {
+    private static Scene scene;
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        scene = new Scene(loadFXML("primary"));
+        stage.setScene(scene);
+        stage.setResizable(false);
+        stage.show();
+    }
+
+    static void setRoot(String fxml) throws IOException {
+        scene.setRoot(loadFXML(fxml));
+    }
+
+    private static Parent loadFXML(String fxml) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Gui.class.getResource(fxml + ".fxml"));
+        return fxmlLoader.load();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
 }
