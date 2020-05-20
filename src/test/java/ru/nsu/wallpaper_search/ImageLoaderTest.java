51deb303df1110a35b0462564369f4a51725c105
p.chagina <p.chagina@g.nsu.ru>
2020-05-20 19:40:15 +0700

    added ImageLoader test for load

---
+++ src/test/java/ru/nsu/wallpaper_search/ImageLoaderTest.java
@@ -0,0 +1,35 @@
+package ru.nsu.wallpaper_search;
+
+import org.junit.Assert;
+import org.junit.Test;
+import ru.nsu.wallpaper_search.exceptions.ImageLoadException;
+import ru.nsu.wallpaper_search.tools.ImageLoader;
+import ru.nsu.wallpaper_search.tools.PicCell;
+
+import java.io.IOException;
+
+public class ImageLoaderTest {
+    private static final Class<ImageLoadException> error = ImageLoadException.class;
+
+    private static final String blockedLink = "https://picstatio.com/download/1600x900/utngox/The-Legend" +
+            "-Of-Zelda-Breath-Of-The-Wild-video-game-gaming-art-wallpaper.jpg";
+    private static final String workingLink = "https://www.wallpapermaiden.com/wallpaper/25017/" +
+            "download/1600x900/princess-zelda-elf-ears-the-legend-of-zelda-smiling-blonde-anime-games.jpeg";
+
+    private static final String alternatePath = "C:/Users/%s/WallpaperSearcher/found.jpg";
+
+    @Test
+    public void loadTest() throws IOException {
+        var workingCell = new PicCell("https://im0-tub-ru.yandex.net/i?id=81ef90734d287839980ce545c9dc3696&n=13");
+        workingCell.addOriginal(blockedLink);
+        workingCell.addOriginal(workingLink);
+        workingCell.addOriginal(blockedLink);
+
+        var path = ImageLoader.load(workingCell);
+        var user = System.getProperty("user.name");
+        Assert.assertTrue(String.format(path, user).equals(path)
+                || String.format(alternatePath, user).equals(path));
+    }
+
+
+}
