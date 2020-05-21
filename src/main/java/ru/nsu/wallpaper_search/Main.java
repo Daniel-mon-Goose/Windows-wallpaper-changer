a29172fc0eaa99944f8d00ca39fcf38b31a965cc
Alena Kokorina <alena.kokorina24@gmail.com>
2020-05-21 22:40:47 +0700

    fixed pom.xml

--- src/main/java/ru/nsu/wallpaper_search/Main.java
+++ src/main/java/ru/nsu/wallpaper_search/Main.java
@@ -1,4 +1,10 @@
 package ru.nsu.wallpaper_search;
 
+import ru.nsu.wallpaper_search.gui.GuiController;
+
 public class Main {
+    public static void main(String[] args) {
+        GuiController guiController = new GuiController();
+    }
+
 }
