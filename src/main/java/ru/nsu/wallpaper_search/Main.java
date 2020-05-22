22353f3a46e51ea8486465684086a4e18789ae30
Alena Kokorina <alena.kokorina24@gmail.com>
2020-05-23 01:41:44 +0700

    removed unnecessary import

--- src/main/java/ru/nsu/wallpaper_search/Main.java
+++ src/main/java/ru/nsu/wallpaper_search/Main.java
@@ -2,11 +2,9 @@ package ru.nsu.wallpaper_search;
 
 import ru.nsu.wallpaper_search.gui.GuiController;
 
-import java.io.IOException;
-
 public class Main {
-    public static void main(String[] args) throws IOException {
-            GuiController guiController = new GuiController();
+    public static void main(String[] args) {
+        GuiController guiController = new GuiController();
     }
 
 }
