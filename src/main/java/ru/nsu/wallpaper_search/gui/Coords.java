09016aa2a4afd75a1a0819f136524e4a2052479b
София Паршукова <s.parshukova@g.nsu.ru>
2020-05-23 02:50:25 +0700

    added select picture listener temlate

---
+++ src/main/java/ru/nsu/wallpaper_search/gui/Coords.java
@@ -0,0 +1,20 @@
+package ru.nsu.wallpaper_search.gui;
+
+public class Coords {
+    private int x;
+    private int y;
+
+    public Coords(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+}
