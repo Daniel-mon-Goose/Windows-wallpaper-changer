009a3c6d60146732168e4b54a2d8e8d84c19e8f0
София Паршукова <s.parshukova@g.nsu.ru>
2020-05-22 17:41:35 +0700

    added window resizer for search results & little fixes

--- src/main/java/ru/nsu/wallpaper_search/gui/Gui.java
+++ src/main/java/ru/nsu/wallpaper_search/gui/Gui.java
@@ -3,51 +3,58 @@ package ru.nsu.wallpaper_search.gui;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionListener;
-import java.awt.event.FocusEvent;
 import java.awt.event.FocusListener;
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
 
 public class Gui extends JFrame {
 
-    private JPanel contentPane, prefPane, queryPane, resultsPane;
+    private JPanel contentPane, prefPane, queryPane, galleryPane;
     private JComboBox<Integer> widthBox, heightBox;
-    private JScrollPane pictures;
     private JLabel widthLabel, heightLabel;
     private JTextField queryField;
     private JButton searchButton;
-    private int viewWidth, viewHeight;
+    private JScrollPane resultsPane;
+
+    private int startWidth, startHeight, showResultHeight;
+    private int cellSize;
+    private int spaceSize;
+    private BufferedImage gallery;
 
     public JTextField getQueryField() {
         return queryField;
     }
 
     public Gui() {
+        add(contentPane);
         setContentPane(contentPane);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setTitle("Wallpaper search");
 
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-        viewWidth = (int)(screenSize.getWidth() * 0.45);
-        viewHeight = (int)(screenSize.getHeight() * 0.6);
-        this.setPreferredSize(new Dimension(viewWidth, viewHeight));
-        this.setResizable(false);
+        startWidth = (int) (screenSize.getWidth() * 0.3);
+        startHeight = (int) (screenSize.getHeight() * 0.2);
+        setPreferredSize(new Dimension(startWidth, startHeight));
+        prefPane.setPreferredSize(new Dimension(startWidth, (int)(startHeight * 0.7)));
+        setResizable(false);
 
-        resultsPane.setPreferredSize(new Dimension(viewWidth, (int)(viewHeight * 0.5)));
-
-        widthBox.addItem((int)screenSize.getWidth());
-        heightBox.addItem((int)screenSize.getHeight());
+        widthBox.addItem((int) screenSize.getWidth());
+        heightBox.addItem((int) screenSize.getHeight());
         int[] widthItems = {1920, 1366, 1536};
         int[] heightItems = {1080, 768, 864};
         for (int widthItem : widthItems) {
-            if (!(widthItem == (int)screenSize.getWidth())) widthBox.addItem(widthItem);
+            if (!(widthItem == (int) screenSize.getWidth())) widthBox.addItem(widthItem);
         }
         for (int heightItem : heightItems) {
-            if (!(heightItem == (int)screenSize.getHeight())) {
+            if (!(heightItem == (int) screenSize.getHeight())) {
                 heightBox.addItem(heightItem);
             }
         }
     }
 
-    public void addSearshListener(ActionListener l) { searchButton.addActionListener(l);}
+    public void addSearshListener(ActionListener l) {
+        searchButton.addActionListener(l);
+    }
 
     public void createPlaceholder(String placeholder, FocusListener f) {
         queryField.setText(placeholder);
@@ -67,4 +74,44 @@ public class Gui extends JFrame {
         return field.getText();
     }
 
+    public void resizeWindow() {
+        setResizable(true);
+        showResultHeight = startHeight * 3 - 100;
+        resize(new Dimension(startWidth, showResultHeight));
+        prefPane.setPreferredSize(new Dimension(startWidth, (int)(showResultHeight * 0.4)));
+        setResizable(false);
+    }
+
+
+    public void addResultsPane() {
+        resizeWindow();
+        galleryPane = new JPanel() {
+            @Override
+            public void paintComponent(Graphics g) {
+                super.paintComponent(g);
+                g.drawImage(gallery, 0, 0, this);
+            }
+
+        };
+
+        resultsPane = new JScrollPane(galleryPane);
+        SpringLayout layout = new SpringLayout();
+        setLayout(layout);
+        layout.putConstraint(SpringLayout.SOUTH, resultsPane, 0, SpringLayout.SOUTH, contentPane);
+        layout.putConstraint(SpringLayout.NORTH, prefPane, 0, SpringLayout.NORTH, contentPane);
+        layout.putConstraint(SpringLayout.NORTH, queryPane, 0, SpringLayout.SOUTH, prefPane);
+        contentPane.add(resultsPane, layout);
+
+        resultsPane.setVisible(true);
+        galleryPane.setVisible(true);
+        resultsPane.setPreferredSize(new Dimension(startWidth, (int) (showResultHeight * 0.5)));
+        galleryPane.setPreferredSize(new Dimension(startWidth-50, (int) (showResultHeight * 0.8)));
+
+        cellSize = (int) (startWidth * 0.2);
+        spaceSize = (int) (startWidth * 0.06);
+    }
+
+    public void drawImages(ArrayList<Image> images) {
+        addResultsPane();
+    }
 }
