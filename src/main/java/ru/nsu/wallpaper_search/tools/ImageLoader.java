b9728ab8aa402da5fbbe89756afdcaf0c9830382
p.chagina <p.chagina@g.nsu.ru>
2020-05-23 00:58:37 +0700

    code quality fix

--- src/main/java/ru/nsu/wallpaper_search/tools/ImageLoader.java
+++ src/main/java/ru/nsu/wallpaper_search/tools/ImageLoader.java
@@ -35,7 +35,7 @@ public class ImageLoader {
         for (var link: links.getOriginals()) {
             try (var netStream = new URL(link).openStream()) {
                 flag = Files.copy(netStream, Paths.get(datPath), StandardCopyOption.REPLACE_EXISTING);
-            } catch (IOException e) { System.out.println(e.getMessage()); }
+            } catch (IOException e) { flag = -1; }
 
             if (flag != -1) {
                 return datPath;
