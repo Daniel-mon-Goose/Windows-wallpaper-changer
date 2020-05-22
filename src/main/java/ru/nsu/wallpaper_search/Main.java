f5ca2a67c84d933e6ccbe6e42257d448b80a0627
Alena Kokorina <alena.kokorina24@gmail.com>
2020-05-23 02:56:43 +0700

    removed unnecessary imports and variable

--- src/main/java/ru/nsu/wallpaper_search/Main.java
+++ src/main/java/ru/nsu/wallpaper_search/Main.java
@@ -2,13 +2,9 @@ package ru.nsu.wallpaper_search;
 
 import ru.nsu.wallpaper_search.gui.GuiController;
 
-import javax.swing.*;
-import java.awt.*;
-import java.util.ArrayList;
-
 public class Main {
     public static void main(String[] args) {
-        GuiController guiController = new GuiController(()-> System.out.println("notify"));
+        new GuiController(()-> System.out.println("notify"));
     }
 
 }
