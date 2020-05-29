b230a276f3ee691651be5158af785260d8dc5565
Alena Kokorina <alena.kokorina24@gmail.com>
2020-05-29 21:11:17 +0700

    some refactoring

--- src/main/java/ru/nsu/wallpaper_search/Main.java
+++ src/main/java/ru/nsu/wallpaper_search/Main.java
@@ -8,6 +8,6 @@ public class Main {
     public static void main(String[] args) {
         var dh = new DataHandler();
         var ch = new ChangeHandler();
-        GuiController g = new GuiController(dh, ch);
+        new GuiController(dh, ch);
     }
 }
--- src/main/java/ru/nsu/wallpaper_search/gui/Gui.java
+++ src/main/java/ru/nsu/wallpaper_search/gui/Gui.java
@@ -56,7 +56,7 @@ public class Gui extends JFrame {
         prefPane.setPreferredSize(new Dimension(paneWidth, (int)(paneHeight * 0.2)));
         buttonPane.setPreferredSize(new Dimension(paneWidth, (int)(paneHeight * 0.6)));
         queryPane.setPreferredSize(new Dimension(paneWidth, (int)(paneHeight * 0.2)));
-        //setResizable(false);
+        setResizable(false);
 
         setButtonsDesign();
 
