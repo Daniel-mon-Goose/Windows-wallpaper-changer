52209e1bd6f6aac04f46d94424787d94f4b8c795
Denis Protasov <d.protasov@g.nsu.ru>
2020-05-30 03:09:04 +0700

    deleted commented code

--- src/main/java/ru/nsu/wallpaper_search/scraper/Scraper.java
+++ src/main/java/ru/nsu/wallpaper_search/scraper/Scraper.java
@@ -12,7 +12,6 @@ public class Scraper {
 
         var doc = Extractor.accessPicturesPage(size, theme);
         var result = Extractor.getPictures(doc);
-        //result.addAll(Extractor.getPictures(doc));
 
         doc = Extractor.accessPicturesPage(size, theme.concat(" wallpaper"));
         result.addAll(Extractor.getPictures(doc));
