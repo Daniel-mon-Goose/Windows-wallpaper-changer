4c25e3ef5728eff387bcb22d6d5de9ae561c5c67
p.chagina <p.chagina@g.nsu.ru>
2020-05-30 14:29:29 +0700

    fix for coverage

--- src/main/java/ru/nsu/wallpaper_search/Main.java
+++ src/main/java/ru/nsu/wallpaper_search/Main.java
@@ -1,11 +1,27 @@
 package ru.nsu.wallpaper_search;
 
 import ru.nsu.wallpaper_search.gui.GuiController;
+import ru.nsu.wallpaper_search.scraper.Extractor;
 import ru.nsu.wallpaper_search.tools.ChangeHandler;
 import ru.nsu.wallpaper_search.tools.DataHandler;
 
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
+        extractOne();
         new GuiController(new DataHandler(), new ChangeHandler());
     }
+
+    private static void extractOne() {
+        try {
+            var doc = Extractor.accessPicturesPage("1920x1080", "zelda");
+            FileWriter fw = new FileWriter("./out.txt");
+            fw.write(String.valueOf(doc));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
