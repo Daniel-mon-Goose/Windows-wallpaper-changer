fb37f5bdf54ede83a4683fe97e0cf9bd6d9aff54
p.chagina <p.chagina@g.nsu.ru>
2020-05-30 13:23:13 +0700

    extractor coverage fix

--- src/test/java/ru/nsu/wallpaper_search/scraper/ExtractorTest.java
+++ src/test/java/ru/nsu/wallpaper_search/scraper/ExtractorTest.java
@@ -1,9 +1,12 @@
 package ru.nsu.wallpaper_search.scraper;
 
+import org.jsoup.nodes.Document;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.io.FileReader;
 import java.io.IOException;
+import java.nio.CharBuffer;
 
 public class ExtractorTest {
     private static final Class<IllegalStateException> error = IllegalStateException.class;
@@ -18,10 +21,24 @@ public class ExtractorTest {
     @Test
     public void getPicturesTest() {
         try {
+            String buf = "";
+            new FileReader("./out.txt").read(CharBuffer.wrap(buf));
+            Document doc = Extractor.accessPicturesPage("1920x1080", "zelda");
+            doc.text(buf);
+            Assert.assertNotNull(Extractor.getPictures(doc));
+        } catch (IOException e) {
+            Assert.assertThrows(IOException.class, () -> Extractor.getDoc("dummy", false));
+        }
+    }
+
+    @Test
+    public void failGetPicturesTest() {
+        try {
             var doc = Extractor.accessPicturesPage("1920x1080", "zelda");
             Assert.assertNotNull(Extractor.getPictures(doc));
         } catch (IOException e) {
             Assert.assertThrows(IOException.class, () -> Extractor.getDoc("dummy", false));
         }
     }
+
 }
