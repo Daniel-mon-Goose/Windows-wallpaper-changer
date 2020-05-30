88b444554ccec54437edfbd39177dbd4f75b461d
p.chagina <p.chagina@g.nsu.ru>
2020-05-30 12:03:48 +0700

    add Extractor getPicsTest

--- src/test/java/ru/nsu/wallpaper_search/scraper/ExtractorTest.java
+++ src/test/java/ru/nsu/wallpaper_search/scraper/ExtractorTest.java
@@ -3,6 +3,8 @@ package ru.nsu.wallpaper_search.scraper;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.io.IOException;
+
 public class ExtractorTest {
     private static final Class<IllegalStateException> error = IllegalStateException.class;
 
@@ -11,5 +13,15 @@ public class ExtractorTest {
         Assert.assertThrows(error, () -> {
             var instance = new Extractor();
         });
+    }
+
+    @Test
+    public void getPicturesTest() {
+        try {
+            var doc = Extractor.accessPicturesPage("1920x1080", "zelda");
+            Assert.assertNotNull(Extractor.getPictures(doc));
+        } catch (IOException e) {
+            Assert.assertThrows(IOException.class, () -> Extractor.getDoc("dummy", false));
+        }
     }
 }
